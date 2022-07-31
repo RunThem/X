360c8e23b8b5c448ad09f47d96bb486d83eb7392
RunThem <iccy.fun@outlook.com>
2022-07-31 22:14:15 +0800

    modify UULP/2/cp.c
    Signed-off-by: RunThem <iccy.fun@outlook.com>

--- UULP/2/cp.c
+++ UULP/2/cp.c
@@ -7,8 +7,10 @@
  */
 
 #include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #define BUFFERSIZE 4096
@@ -16,29 +18,46 @@
 
 void oops(char*, const char*);
 
+static bool confirm;
+
 int main(int argc, const char* argv[]) {
   int  in_fd, out_fd, n_chars;
   char buf[BUFFERSIZE];
+  int  src_idx = 1;
+  int  dst_idx = 2;
 
   /* check args */
-  if (3 != argc) {
+  if (3 > argc) {
     fprintf(stderr, "usage: %s source destination\n", argv[0]);
     exit(1);
   }
 
-  /* open file */
-  if (-1 == (in_fd = open(argv[1], O_RDONLY))) {
-    oops("Cannot open ", argv[1]);
+  if (!strncmp(argv[1], "-i", strlen("-i"))) {
+    confirm = true;
+    src_idx++;
+    dst_idx++;
   }
 
-  if (-1 == (out_fd = creat(argv[2], COPYMODE))) {
-    oops("Cannot creat ", argv[2]);
+  if (confirm && (0 == access(argv[dst_idx], F_OK))) {
+    fprintf(stderr, "file %s exited.\nwhether to cover(y/n):", argv[dst_idx]);
+    if ('n' == getchar()) {
+      exit(0);
+    }
+  }
+
+  /* open file */
+  if (-1 == (in_fd = open(argv[src_idx], O_RDONLY))) {
+    oops("Cannot open ", argv[src_idx]);
+  }
+
+  if (-1 == (out_fd = creat(argv[dst_idx], COPYMODE))) {
+    oops("Cannot creat ", argv[dst_idx]);
   }
 
   /* copy file */
   while (0 < (n_chars = read(in_fd, buf, BUFFERSIZE))) {
     if (n_chars != write(out_fd, buf, n_chars)) {
-      oops("Write error to ", argv[2]);
+      oops("Write error to ", argv[dst_idx]);
     }
   }
 
